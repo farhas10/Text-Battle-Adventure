32a1cc34ce3b867ce947e06f534237c38d9dbbd9
farzadhasan <farzadhasan@localhost>
2022-06-06 01:57:46 -0700

    HealthPotion Commit

---
+++ TextBattleAdventure/src/HealthPotion.java
@@ -0,0 +1,28 @@
+/**
+ * Name:Farzad Hasan
+ * Last Updated On: 06/2/22
+ * APCS Period 2
+ * Text Battle Project Part TwoB
+ * 
+ * This class is the represents a HealthPotion object which is an Item. It must contain all of the fields and methods 
+ * detailed in the project spec. You 
+ * may add additional fields and methods if you lik.
+ */
+
+public class HealthPotion extends Item {
+	
+	//Basic constructor for the health potion item.
+	public HealthPotion(String rarity){
+		super("Health Potion", rarity);
+	}
+	
+	//Use method for the potion which increasing the current
+	//player health value.
+	public void use(Player player) {
+		int points = super.getPoints();
+		player.healDamage(points);
+		System.out.println(player.getName() + " used a " + super.toString() + " increasing their "
+				+ "available health by " + points + " points.");
+	}
+	
+}
